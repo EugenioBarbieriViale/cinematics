d95fbe64361958dcf9255da07d0a60760c6911e5
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2022-12-07 12:43:17 +0100

    new

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ clock = pygame.time.Clock()
 
 screenx,screeny = 1000,800
 
-window = pygame.display.set_mode([screenx,screeny])
+screen = pygame.display.set_mode([screenx,screeny])
 pygame.display.set_caption("Cinematics")
 
 vel = pygame.math.Vector2(0)
 pos = pygame.math.Vector2(screenx/2, screeny/2)
+
+endPoints = []
+
+R, G, B = 255, 255, 255
 
 while True:
     for event in pygame.event.get():
@@ -18,7 +22,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    window.fill((0,0,0))
+    screen.fill((0,0,0))
 
     mouse_pos = pygame.math.Vector2(pygame.mouse.get_pos())
     distance = mouse_pos - pos 
@@ -27,20 +31,27 @@ while True:
     vel += acc/distance.magnitude()
     pos += vel
 
-    pygame.draw.circle(window, (100,100,100), (pos.x, pos.y), 8)
-    pygame.draw.line(window, (0,255,0), pos, 10*vel+pos)
-    pygame.draw.line(window, (255,0,0), pos+vel*10, acc+10*vel+pos)
+    endPoints.append((pos.x, pos.y))
 
+    for i in range(len(endPoints) - 1):
+            pygame.draw.line(screen, (R, G, B), (int(endPoints[i][0]), int(endPoints[i][1])), (int(endPoints[i + 1][0]), int(endPoints[i + 1][1])), 1)
+
+    pygame.draw.circle(screen, (100,100,100), (pos.x, pos.y), 8)
+    pygame.draw.line(screen, (0,255,0), pos, 10*vel+pos)
+    pygame.draw.line(screen, (255,0,0), pos+vel*10, acc+10*vel+pos)
 
     font = pygame.font.SysFont("Comic Sans MS", 30)
-    # font = pygame.font.SysFont("monospace", 25)
     label_p = font.render("Position: " + str(pos), 1, (255,255,255))
     label_v = font.render("Velocity: " + str(vel), 1, (255,255,255))
     label_a = font.render("Acceleration: " + str(acc), 1, (255,255,255))
 
-    window.blit(label_p, (10,10))
-    window.blit(label_v, (10,30))
-    window.blit(label_a, (10,50))
+    screen.blit(label_p, (10,10))
+    screen.blit(label_v, (10,30))
+    screen.blit(label_a, (10,50))
+
+    R -= 1
+    G -= 1
+    B -= 1
 
     pygame.display.flip()
     clock.tick(60)
